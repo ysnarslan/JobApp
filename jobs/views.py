62a9c45e592f92bdeb0ff92feabe2d7662d0fc47
Yasin Arslan <ysnarsln1730@gmail.com>
2023-11-02 22:23:25 +0300

    refactor: add dynamic html to job detail

--- jobs/views.py
+++ jobs/views.py
@@ -2,13 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+job_title = [
+    'First Job',
+    'Second Job'
+]
+job_description = [
+    'First job description.',
+    'Second job description.'
+]
+
 # Create your views here.
 def hello(request):
-    return HttpResponse('<h3>Hello World!<h3>')
+    return HttpResponse('<h3>Hello World!</h3>')
 
 
 def job_detail(request, id):
-    site = "https://google.com"
+    idx = int(id) - 1
+    return_html = f"<h1>{job_title[idx]}</h1> <h3> {job_description[idx]}"
 
-    return HttpResponse(f"Visit Google from <a href={site} target='_blank'> here <a> ")
+    return HttpResponse(return_html)
 
