efa1d499a35c0519ec3b3c82244393b73fe0a92f
Yasin Arslan <ysnarsln1730@gmail.com>
2023-11-04 00:12:09 +0300

    refactor: use url tag

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", job_list, name='home'),
+    path("", job_list, name='index'),
     path("job/<int:id>", job_detail, name='job_detail'),
     path("hello/", hello, name='hello')
 ]
