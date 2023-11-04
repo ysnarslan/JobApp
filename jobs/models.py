9fed3c49b5f9085ef1bab9842e3abdf1ff232f08
Yasin Arslan <ysnarsln1730@gmail.com>
2023-11-04 14:40:17 +0300

    feat: update Job model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -5,5 +6,16 @@ from django.db import models
 class Job(models.Model):
     job_title = models.CharField(
         max_length=255,
+        help_text="",
+    )
+    job_description = models.TextField(
+        help_text="",
+    )
+    created_date = models.DateField(
+        auto_now_add=True,
+        help_text="",
+    )
+    salary = models.IntegerField(
+        validators=[MinValueValidator(0)],
         help_text="",
     )
