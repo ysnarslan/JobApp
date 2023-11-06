7ef01bfdb04597c7ccd50fbc4db844a0da5015c3
Yasin Arslan <ysnarsln1730@gmail.com>
2023-11-06 17:51:25 +0300

    chore: add slug to Job

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -19,6 +20,13 @@ class Job(models.Model):
         validators=[MinValueValidator(0)],
         help_text="",
     )
+    slug = models.SlugField(
+        null=True,
+    )
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.job_title)
+        return super(Job, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.job_title
