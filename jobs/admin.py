26235eb1b82b5eb8b226d6fa71c6c59f7fad9cac
Yasin Arslan <ysnarsln1730@gmail.com>
2023-11-07 16:10:50 +0300

    feat: update JobAdmin

--- jobs/admin.py
+++ jobs/admin.py
@@ -4,9 +4,22 @@ from jobs.models import Job
 
 # Register your models here.
 class JobAdmin(admin.ModelAdmin):
-    list_display = ["id", "job_title", "salary", "created_date"]
+    list_display = ["id", "job_title", "job_description", "salary", "created_date"]
+    list_filter = ["salary", "created_date", "expiry"]
     # list_editable = []
-    # search_fields = []
+    search_fields = ["job_title", "salary", "job_description"]
+    search_help_text = "Write in your query and hit search"
+    # fields = ["job_title", "job_description", "salary"]
+    # exclude = []
+    fieldsets = (
+        ("Basic info", {
+            "fields": ["job_title", "job_description"]
+        }),
+        ("More info", {
+            "fields": ["expiry", "salary", "slug"],
+            "classes": ["collapse"],
+        }),
+    )
 
     class Meta:
         model = Job
