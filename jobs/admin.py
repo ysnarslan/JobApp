ef9272818a7e09faa6e5f61705076d740e791cf8
Yasin Arslan <ysnarsln1730@gmail.com>
2023-11-07 14:40:39 +0300

    feat: update JobAdmin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-
 from jobs.models import Job
 
+
 # Register your models here.
-admin.site.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ["id", "job_title", "salary", "created_date"]
+    # list_editable = []
+    # search_fields = []
+
+    class Meta:
+        model = Job
+
+
+admin.site.register(Job, JobAdmin)
