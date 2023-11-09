f57c686f7951e4815cc0e16df49534d50526c8f7
Yasin Arslan <ysnarsln1730@gmail.com>
2023-11-09 15:34:11 +0300

    feat: create subscribe app

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Subscribe(models.Model):
+    first_name = models.CharField(
+        max_length=255,
+    )
+    last_name = models.CharField(
+        max_length=255,
+    )
+    email = models.CharField(
+        max_length=255,
+    )
--- subscribe/views.py
+++ subscribe/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 
+from subscribe.models import Subscribe
+
 
 # Create your views here.
 def subscribe(request):
-    context = {}
+    empty_email_error = ""
+    if request.POST:
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        email = request.POST['email']
+        print(email)
+        if email == "":
+            empty_email_error = "No email entered"
+
+        subscribe = Subscribe(
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+        subscribe.save()
+
+    context = {
+        'empty_email_error': empty_email_error,
+    }
 
     return render(request, "subscribe/subscribe.html", context)
