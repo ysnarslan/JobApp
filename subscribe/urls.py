970fe07d8e378b3e284738b8d941ab1278f870d3
Yasin Arslan <ysnarsln1730@gmail.com>
2023-11-09 16:46:25 +0300

    feat: create subscribe and thank you page

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import subscribe
+from .views import subscribe, thank_you
 
 urlpatterns = [
-    path("subscribe/", subscribe, name="subscribe")
+    path("subscribe/", subscribe, name="subscribe"),
+    path("thank_you/", thank_you, name='thank_you'),
 ]
--- subscribe/views.py
+++ subscribe/views.py
@@ -1,28 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
+from subscribe.forms import SubscribeForm
 from subscribe.models import Subscribe
 
 
 # Create your views here.
-def subscribe(request):
-    empty_email_error = ""
-    if request.POST:
-        first_name = request.POST['firstname']
-        last_name = request.POST['lastname']
-        email = request.POST['email']
-        print(email)
-        if email == "":
-            empty_email_error = "No email entered"
 
-        subscribe = Subscribe(
-            first_name=first_name,
-            last_name=last_name,
-            email=email
-        )
-        subscribe.save()
+def thank_you(request):
+    context = {}
+    return render(request, "subscribe/thank_you.html", context)
+
+def subscribe(request):
+    subscribe_form = SubscribeForm()
+    if request.POST:
+        subscribe_form = SubscribeForm(request.POST)
+        if subscribe_form.is_valid():
+            print("Valid form.")
+            print(subscribe_form.cleaned_data)
+            first_name = subscribe_form.cleaned_data["first_name"]
+            last_name = subscribe_form.cleaned_data["last_name"]
+            email = subscribe_form.cleaned_data["email"]
+            subscribe = Subscribe(
+                first_name=first_name,
+                last_name=last_name,
+                email=email
+            )
+            subscribe.save()
+            return redirect(reverse("thank_you"))
 
     context = {
-        'empty_email_error': empty_email_error,
+        'subscribe_form': subscribe_form,
     }
 
     return render(request, "subscribe/subscribe.html", context)
