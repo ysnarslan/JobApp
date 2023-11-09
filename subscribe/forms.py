423c8716576681baf269d61a8dc72bb04f7f8217
Yasin Arslan <ysnarsln1730@gmail.com>
2023-11-09 19:27:59 +0300

    refactor: change Subscribe form to Subscribe modelform

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -1,31 +1,53 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
+
+from subscribe.models import Subscribe
 
 
-def validate_comma(value):
-    if "," in value:
-        raise forms.ValidationError("Invalid last name.")
-    return value
+class SubscribeForm(forms.ModelForm):
+    class Meta:
+        model = Subscribe
+        fields = '__all__'
+        # exclude = ("first_name", )
+        labels = {
+            "first_name": _("First Name:"),
+            "last_name": _("Last Name:"),
+            "email": _("E-mail"),
+        }
+        help_texts = {
+            "first_name": _("Enter your first name. (Only character)")
+        }
+        error_messages = {
+            "first_name": {
+                "required": _("You cannot move forward without first name")
+            },
+        }
 
-class SubscribeForm(forms.Form):
-    first_name = forms.CharField(
-        max_length=255,
-        label="First Name:",
-        required=True,
-        help_text="Enter your first name. (Only character)",
-    )
-    last_name = forms.CharField(
-        max_length=255,
-        label="Last Name:",
-        disabled=False,
-        validators=[validate_comma],
-    )
-    email = forms.EmailField(
-        max_length=255,
-        label="E-mail:",
-    )
-
-    def clean_first_name(self):
-        data = self.cleaned_data["first_name"]
-        if "," in data:
-            raise forms.ValidationError("Invalid first name.")
-        return data
+# def validate_comma(value):
+#     if "," in value:
+#         raise forms.ValidationError("Invalid last name.")
+#     return value
+#
+# class SubscribeForm(forms.Form):
+#     first_name = forms.CharField(
+#         max_length=255,
+#         label="First Name:",
+#         required=True,
+#         help_text="Enter your first name. (Only character)",
+#     )
+#     last_name = forms.CharField(
+#         max_length=255,
+#         label="Last Name:",
+#         disabled=False,
+#         validators=[validate_comma],
+#     )
+#     email = forms.EmailField(
+#         max_length=255,
+#         label="E-mail:",
+#     )
+#
+#     def clean_first_name(self):
+#         data = self.cleaned_data["first_name"]
+#         if "," in data:
+#             raise forms.ValidationError("Invalid first name.")
+#         return data
--- subscribe/views.py
+++ subscribe/views.py
@@ -16,17 +16,18 @@ def subscribe(request):
     if request.POST:
         subscribe_form = SubscribeForm(request.POST)
         if subscribe_form.is_valid():
-            print("Valid form.")
-            print(subscribe_form.cleaned_data)
-            first_name = subscribe_form.cleaned_data["first_name"]
-            last_name = subscribe_form.cleaned_data["last_name"]
-            email = subscribe_form.cleaned_data["email"]
-            subscribe = Subscribe(
-                first_name=first_name,
-                last_name=last_name,
-                email=email
-            )
-            subscribe.save()
+            subscribe_form.save()
+            # print("Valid form.")
+            # print(subscribe_form.cleaned_data)
+            # first_name = subscribe_form.cleaned_data["first_name"]
+            # last_name = subscribe_form.cleaned_data["last_name"]
+            # email = subscribe_form.cleaned_data["email"]
+            # subscribe = Subscribe(
+            #     first_name=first_name,
+            #     last_name=last_name,
+            #     email=email
+            # )
+            # subscribe.save()
             return redirect(reverse("thank_you"))
 
     context = {
