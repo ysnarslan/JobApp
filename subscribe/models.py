aed0bb54f79d80aa93fc705c75daa8589e93b96f
Yasin Arslan <ysnarsln1730@gmail.com>
2023-11-09 19:50:56 +0300

    feat: add option field to Subscribe model

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+NEWSLETTER_OPTION = [
+    ("W", "Weekly"),
+    ("M", "Monthly"),
+]
 
 # Create your models here.
 class Subscribe(models.Model):
@@ -11,4 +15,9 @@ class Subscribe(models.Model):
     )
     email = models.EmailField(
         max_length=255,
+    )
+    option = models.CharField(
+        max_length=2,
+        choices=NEWSLETTER_OPTION,
+        default="M",
     )
