e7028ac7f3976a134147cca5bd58b57c80c10f3a
Yasin Arslan <ysnarsln1730@gmail.com>
2023-11-16 22:43:32 +0300

    feat: add static url

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -19,9 +19,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('jobs.urls')),
-    path('', include('subscribe.urls')),
-    path('uploads/', include('uploadpp.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [path('admin/', admin.site.urls),
+               path('', include('jobs.urls')),
+               path('', include('subscribe.urls')),
+               path('uploads/', include('uploadpp.urls'))
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
